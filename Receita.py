1e7c1f5d7d727950167a182165d3609f76a3854c
Davi  Ravedutti <davi.serpa.r.santos@gmail.com>
2025-09-14 20:21:46 -0300

    Create Receita.py

---
+++ Receita.py
@@ -0,0 +1,73 @@
+# Primeira Ideia do Codigo de Receita
+nome_receita_original = "Picanha Selada"
+print("Nome da Receita: Picanha Selada\n")
+print("Ingredientes usados na Receita:\n")
+
+ingrediente1 = "Picanha"
+ingrediente2 = "Sal Grosso"
+ingrediente3 = "Oregano\n"
+print(ingrediente1)
+print(ingrediente2)
+print(ingrediente3)
+
+quantidade_dos_ingredientes = input("Digite a quantidade de Igredientes da Receita: ")
+quantidade_do_ingrediente = float(input("Quantidade de Picanha em Kg: "))
+quantidade_do_ingrediente = float(input("Quantidade de Sal Grosso em Gramas: "))
+quantidade_do_ingrediente = float(input("Quantidade de Oregano em Gramas:"))
+
+quantidade_de_pessoas_servidas = float(input("Digite a Quantidade de pessoas que serve: "
+
+                                             
+
+# Novo Incio do Codigo e Identificação do que vou fazer nele
+nome_nova_receita = "Picanha em Bifes"
+print("Nome da Receita: Picanha em Bifes\n")
+
+# Declarçaõ de Variaveis String
+print("Ingredientes usados na Receita:\n")
+ingrediente1 = "Picanha"
+ingrediente2 = "Sal Grosso"
+ingrediente3 = "Oregano"
+print(ingrediente1)
+print(ingrediente2)
+print(ingrediente3)
+
+#Declaração de Variaveis Int
+picanha = 75
+sal_grosso = 125
+oregano = 45
+quantidade_de_cada_pessoa = 10
+print("")
+
+# Realização de Calculo das Variaveis Int e String para encontrar o valor exato unitario de cada ingrediente por pessoa
+print("Quantidade dos Igredientes por Pessoa")
+resultado_por_pessoa = picanha / quantidade_de_cada_pessoa
+resultado_por_pessoa2 = sal_grosso / quantidade_de_cada_pessoa
+resultado_por_pessoa3 = oregano / quantidade_de_cada_pessoa
+
+# Demonstrativo do Calculo Realizado
+print(f"Quantidade de Picanha {resultado_por_pessoa} Kg")
+print(f"Quantidade de Sal Grosso {resultado_por_pessoa2} Gramas")
+print(f"Quantidade de Sal Oregano {resultado_por_pessoa3} Gramas")
+print("")
+
+# Realização do Calculo das Variaveis da nova quantidade de pessoas para essa nova receita
+print("Nova quantidade de Ingredientes")
+print("")
+quantidade_de_pessoas_servidas_da_nova_receita = 100
+resultado_nova_receita = resultado_por_pessoa * quantidade_de_pessoas_servidas_da_nova_receita
+resultado_nova_receita2 = resultado_por_pessoa2 * quantidade_de_pessoas_servidas_da_nova_receita
+resultado_nova_receita3 = resultado_por_pessoa3 * quantidade_de_pessoas_servidas_da_nova_receita
+
+# Demonstrativo do Calculo Realizado da Nova Quantidade de Pessoas da Nova Receita
+print(f"Quantidade de Picanha Nova Receita {resultado_nova_receita} Kg")
+print(f"Quantidade de Sal Grosso Nova Receita {resultado_nova_receita2} Gramas")
+print(f"Quantidade de Sal Oregano {resultado_nova_receita3} Gramas")
+print("")
+
+# Demonstrativo da nova quantidade de Pessoas
+print("Nova Quantidade de Pessoas Servida da Nova Receita: 100")
+print("")
+
+# Valor Gasto para a nova Receita
+print("Valor Total Gasto na Nova Receita: R$ 950,00")
